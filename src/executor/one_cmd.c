1ce85565de2a01adfecaea0f2ef30cd85e65cae8
Tobias Anselmo <tanselmo@c3r4s3.42malaga.com>
2024-08-16 20:31:53 +0200

    Add 'is_builtin' and 'one_cmd' function (unfinished)

--- src/executor/one_cmd.c
+++ src/executor/one_cmd.c
@@ -27,17 +27,61 @@ static char	*find_cmd(char **path, char *cmd)
 	return (NULL);
 }
 
+static int	is_builtin(t_msh *msh)
+{
+	t_cmd	*cmd;
+
+	cmd = msh->cmd;
+	if (!ft_strncmp("cd", cmd->argv[0], 3))
+		return (0);
+	if (!ft_strncmp("echo", cmd->argv[0], 5))
+		return (ft_echo(msh, msh->cmd->fd_out), 0);
+	if (!ft_strncmp("export", cmd->argv[0], 7))
+		return (0);
+	if (!ft_strncmp("unset", cmd->argv[0], 6))
+		return (0);
+	if (!ft_strncmp("pwd", cmd->argv[0], 4))
+		return (ft_pwd(msh), 0);
+	if (!ft_strncmp("exit", cmd->argv[0], 5))
+		return (ft_exit(msh), 0);
+	if (!ft_strncmp("env", cmd->argv[0], 4))
+		return (ft_env(msh), 0);
+	return (1);
+}
+
 void	one_cmd(t_msh *msh, char **env)
 {
+	// int		fd_in;
+	// int		fd_out;
 	char	*cmd_and_path;
 	char	*cmd;
+	pid_t	pid;
 
 	if (msh->cmd->error == 1)
-		return ;
-	cmd = msh->cmd->argv[0];
-	cmd_and_path = find_cmd(env, cmd);
-	if (!cmd_and_path)
-		error_msh("Command not found", msh, 127);
-	printf("ruta: %s\n", cmd_and_path);
-	return ;
+		return ; /* Not finished */
+	if (is_builtin(msh) == 0)
+	 	return ;
+	else
+	{
+		cmd = msh->cmd->argv[0];
+		cmd_and_path = find_cmd(env, cmd);
+		if (!cmd_and_path)
+		{
+			error_msh("Minishell: Command not found", msh, 127);
+			return ;
+		}
+		pid = fork();
+		if (pid < 0)
+			error_msh("Error creating pid", msh, 0); /* Test return */
+		if (pid == 0)
+		{
+			if (msh->cmd->fd_in != 0)
+				dup2(msh->cmd->fd_in, STDIN_FILENO);
+			if (msh->cmd->fd_out != 1)
+				dup2(msh->cmd->fd_out, STDOUT_FILENO);
+			execve(cmd_and_path, msh->cmd->argv, msh->envp);
+		}
+		waitpid(pid, NULL, 0);
+		free(cmd_and_path);
+	}
 }
