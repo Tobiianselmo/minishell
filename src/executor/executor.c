c445788846639332c9bd66e24fa8ec2f30b09679
Tobias Anselmo <tanselmo@c3r4s3.42malaga.com>
2024-08-16 20:30:24 +0200

    Remove is_builtin from executor

--- src/executor/executor.c
+++ src/executor/executor.c
@@ -1,34 +1,12 @@
 #include "../../includes/minishell.h"
 
-static int	is_builtin(t_msh *msh)
-{
-	t_cmd	*cmd;
-
-	cmd = msh->cmd;
-	if (!ft_strncmp("cd", cmd->argv[0], 3))
-		return (0);
-	if (!ft_strncmp("echo", cmd->argv[0], 5))
-		return (ft_echo(msh, msh->cmd->fd_out), 0);
-	if (!ft_strncmp("export", cmd->argv[0], 7))
-		return (0);
-	if (!ft_strncmp("unset", cmd->argv[0], 6))
-		return (0);
-	if (!ft_strncmp("pwd", cmd->argv[0], 4))
-		return (ft_pwd(msh), 0);
-	if (!ft_strncmp("exit", cmd->argv[0], 5))
-		return (ft_exit(msh), 0);
-	if (!ft_strncmp("env", cmd->argv[0], 4))
-		return (ft_env(msh), 0);
-	return (1);
-}
-
 void	executor(t_msh *msh)
 {
+	g_signal = 1;
 	msh->path = get_path(msh);
 	if (!msh->path)
 		error_msh("Path not found", msh, 130);
-	if (is_builtin(msh) == 0)
-		return ;
-	if (msh->cmd_len == 1)
+	else if (msh->cmd_len == 1)
 		one_cmd(msh, msh->path);
+	g_signal = 0;
 }
