3dedee52923534225b8c2747928bfe582808aa4d
Axel Soubielle Almada <axlsoubielle@gmail.com>
2024-08-19 11:08:04 +0200

    Add error handler for chdir

--- src/executor/cd.c
+++ src/executor/cd.c
@@ -1,11 +1,11 @@
 #include "../../includes/minishell.h"
 
-static int	cmd_argc(t_msh *msh)
+static int	cmd_argc(t_cmd *cmd)
 {
 	int		i;
 
 	i = 0;
-	while (msh->cmd->argv[i])
+	while (cmd->argv[i])
 		i++;
 	return (i);
 }
@@ -25,6 +25,7 @@ static	void	set_env(t_msh *msh, char *var, char *new)
 		}
 		aux = aux->next;
 	}
+	free(new);
 }
 
 static char	*get_env_cd(t_msh *msh, char *var)
@@ -43,11 +44,10 @@ static char	*get_env_cd(t_msh *msh, char *var)
 
 void	cd(t_msh *msh)
 {
-	char	*oldpwd;
 	char	*pwd;
 
-	oldpwd = getcwd(NULL, 0);
-	if (cmd_argc(msh) > 2)
+	pwd = getcwd(NULL, 0);
+	if (cmd_argc(msh->cmd) > 2)
 		error_msh("Minishell: cd: too many arguments", msh, 1);
 	else if (msh->cmd->argv[1] == NULL)
 		chdir(get_env_cd(msh, "HOME"));
@@ -57,10 +57,16 @@ void	cd(t_msh *msh)
 		ft_printf("%s\n", get_env_cd(msh, "OLDPWD"));
 	}
 	else
-		chdir(msh->cmd->argv[1]);
-	set_env(msh, "OLDPWD", oldpwd);
+	{
+		if (chdir(msh->cmd->argv[1]) == -1)
+		{
+			ft_printf("Minishell: cd: %s: %s\n", msh->cmd->argv[1],
+				strerror(errno));
+			msh->state = 1;
+		}
+
+	}
+	set_env(msh, "OLDPWD", pwd);
 	pwd = getcwd(NULL, 0);
 	set_env(msh, "PWD", pwd);
-	free(oldpwd);
-	free(pwd);
 }
