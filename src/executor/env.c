49cadd2c07773631ce771be89653c4f953f07451
Tobias Anselmo <tanselmo@c1r8s4.42malaga.com>
2024-08-28 12:58:44 +0200

    Segfault solved

--- src/executor/env.c
+++ src/executor/env.c
@@ -54,17 +54,19 @@ void	ft_env(t_msh *msh)
 {
 	t_env	*tmp;
 	int		fd;
-	
+
 	tmp = msh->env;
 	fd = msh->cmd->fd_out;
 	if (!tmp)
 		return ;
 	while (tmp)
 	{
-		ft_putstr_fd(tmp->type, fd);
-		ft_putstr_fd("=", fd);
-		ft_putendl_fd(tmp->content, fd);
+		if (tmp->content)
+		{
+			ft_putstr_fd(tmp->type, fd);
+			ft_putstr_fd("=", fd);
+			ft_putendl_fd(tmp->content, fd);
+		}
 		tmp = tmp->next;
 	}
-	msh->cmd = msh->cmd->next;
 }
