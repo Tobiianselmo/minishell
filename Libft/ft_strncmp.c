c57f0ca60f64acf8e056590d3ef16a2d0ee0904d
Tobias Anselmo <tanselmo@c1r3s2.42malaga.com>
2024-09-06 10:13:59 +0200

    Update ft_strncmp.c

--- Libft/ft_strncmp.c
+++ Libft/ft_strncmp.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strncmp.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tanselmo <tanselmo@student.42.fr>          +#+  +:+       +#+        */
+/*   By: tanselmo <tanselmo@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/27 15:15:52 by tanselmo          #+#    #+#             */
-/*   Updated: 2023/12/04 16:15:51 by tanselmo         ###   ########.fr       */
+/*   Updated: 2024/09/05 18:45:18 by tanselmo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,10 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	size_t	i;
 
 	i = 0;
+	if (!s1)
+		return (-1);
+	if (!s2)
+		return (1);
 	while ((s1[i] != '\0' || s2[i] != '\0') && (i != n))
 	{
 		if ((unsigned char)s1[i] > (unsigned char)s2[i])
