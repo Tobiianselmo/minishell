91b1ce77711d3caf88c75d8a4375ea01ad49326b
Tobias Anselmo <tanselmo@c1r3s2.42malaga.com>
2024-09-06 10:18:48 +0200

    Add 'Minishell: ' in error messagge

--- src/parser/errors.c
+++ src/parser/errors.c
@@ -8,6 +8,7 @@ void	error_msh(char *msg, t_msh *msh, int state)
 
 void	error_files(char *name, char *msg)
 {
+	ft_putstr_fd("Minishell: ", 2);
 	ft_putstr_fd(name, 2);
 	ft_putstr_fd(": ", 2);
 	ft_putendl_fd(msg, 2);
